e374e4eb2ee01793d3dd1eed5c38ab204ba005d8
intothemetaverse <jclut49@gmail.com>
2023-01-09 22:57:04 -0500

    commit on master

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,13 +1,43 @@
 import { View, Text, StyleSheet } from 'react-native'
-import Title from '../components/Title'
-function GameScreen () {
+import { useState } from 'react';
+import Title from '../components/ui/Title'
+import NumberContainer from '../components/game/NumberContainer';
+import PrimaryButton from '../components/ui/PrimaryButton';
+
+function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  
+    if (rndNum === exclude) {
+      return generateRandomBetween(min, max, exclude);
+    } else {
+      return rndNum;
+    }
+  }
+
+  let minBoundary = 1;
+  let maxBoundary = 100;
+
+function GameScreen ({userNumber}) {     
+    const initialGuess = generateRandomBetween(1, 100, userNumber)
+    const [currentGuess , setCurrentGuess] = useState(initialGuess);
+
+function nextGuessHandler(direction) { //direction -> lower or higher than guess
+    if (direction === 'lower') {
+        maxBoundary = currentGuess;
+    } else {
+        minBoundary = currentGuess+1;
+    }
+    const newRandNumber= generateRandomBetween(minBoundary, maxBoundary, currentGuess);
+    setCurrentGuess(newRandNumber);
+}
+
     return <View style={styles.screen}>
         <Title>Opponent's Guess</Title>
-        {/*GUESS*/}
+        <NumberContainer>{currentGuess}</NumberContainer>
+        <Text>Higher or Lower?</Text>
         <View>
-            <Text>Higher or Lower?</Text>
-        {/*+
-            -*/}
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
         </View>
             <View>{/*LOG ROUNDS*/}</View>
     </View>
@@ -20,4 +50,4 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 30
     }
-});
+})
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,7 +1,8 @@
 import { TextInput, View, StyleSheet, Alert } from 'react-native';
 import { useState } from 'react'
 
-import PrimaryButton from '../components/PrimaryButton';
+import PrimaryButton from '../components/ui/PrimaryButton';
+
 function StartGameScreen({onPickNumber}) {
     const [enteredNumber, setEnteredNumber] = useState('');
 
